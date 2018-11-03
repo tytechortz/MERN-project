34749770a3bd00e19240db69be0c3040a64146e8
James <tytechortz@gmail.com>
2018-11-03 11:25:58 -0600

    updates server and pck json with cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express        = require('express');
 const app            = express();
 const bodyParser     = require('body-parser');
+const cors           = require('cors');
 
 
 require('./db/db');
@@ -8,6 +9,13 @@ require('./db/db');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 
 const trailController = require('./controllers/trailController');
