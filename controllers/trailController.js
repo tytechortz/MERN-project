75def74d7da26a211658534f9294399d65564a32
James <tytechortz@gmail.com>
2018-11-03 07:11:30 -0600

    sets up get route for 1 trail

--- controllers/trailController.js
+++ controllers/trailController.js
@@ -7,8 +7,27 @@ router.get('/', (req, res) => {
     Trail.find({}, (err, trails) => {
         res.json(trails)
     })
-    console.log('get all')
-})
+    console.log('get request')
+});
 
+router.post('/', (req, res) => {
+    res.status(200).json({
+        message: "handling post request"
+    });
+});
+
+router.get('/:trailId', (req, res) => {
+    const id = req.params.trailId;
+    if (id === 'a'){
+        res.status(200).json({
+            message: 'special',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'an id'
+        });
+    }
+});
 
 module.exports = router; 
